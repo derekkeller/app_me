12e1de6331f85e9c8f9f201457851e9907cfa22f
Derek Keller <derekbkeller@gmail.com>
2011-09-02 12:12:20 -0600

    User relationships (following/unfollowing)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :admin
   before_save :encrypt_password
 
-  has_many :microposts,     :dependent => :destroy
-  has_many :relationships,  :dependent => :destroy,
-                            :foreign_key => "follower_id"
-
+  has_many :microposts,    :dependent => :destroy
+  has_many :relationships, :dependent => :destroy,
+                           :foreign_key => "follower_id"
+  has_many :reverse_relationships, :dependent => :destroy,
+                           :foreign_key => "followed_id",
+                           :class_name => "Relationship"
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   has_many :following, :through => :relationships, :source => :followed
   
   validates :name, :presence => true
@@ -19,6 +22,14 @@ class User < ActiveRecord::Base
 
   def feed
     Micropost.where("user_id = ?", self.id)
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
   end
 
   def self.authenticate(email, submitted_password)
